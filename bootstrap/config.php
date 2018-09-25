39dce853ed6f88cdf19476723e9109081d192880
Donizete Brandão de Alencar <35704534+doni7brandao@users.noreply.github.com>
2018-09-25 15:46:49 -0300

    Add files via upload

---
+++ bootstrap/config.php
@@ -0,0 +1,27 @@
+<?php
+/**********************************************************************************
+ Sistema e-SIC Livre: sistema de acesso a informação baseado na lei de acesso.
+ 
+ Copyright (C) 2014 Prefeitura Municipal do Natal
+ 
+ Este programa é software livre; você pode redistribuí-lo e/ou
+ modificá-lo sob os termos da Licença GPL2.
+***********************************************************************************/
+
+error_reporting(E_ERROR);
+
+define("SISTEMA_NOME", "e-SIC Livre"); //nome do sistema para exibição em lugares diversos
+define("SISTEMA_CODIGO", "esiclivre"); //codigo para definição da lista de sessão do sistema
+
+
+define("MAIL_HOST", "mail.natal.rn.gov.br");
+define("DBHOST", "www.mysql.al.gov.br");
+define("DBUSER", "anadia.al.gov.br");
+define("DBPASS", "KdBAswc6bFAzDSts");
+define("DBNAME", "anadia");
+
+define("SITELNK", "http://www.seusite/esiclivre/");	//endereço principal do site
+
+define("URL_BASE_SISTEMA", "http://www.seusite/esiclivre/");	//endereço principal do site
+
+?>
